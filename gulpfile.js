faf5e1548ef488f3d53b38b8ec368dbccd45a3b9
Annette Lunnon <annette.lunnon@cityrd-bc-501-5.auckland.natcoll.ac.nz>
2015-05-06 11:19:08 +1200

    added task runners

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+// Include Gulp
+
+var gulp = require('gulp');
+
+// Include our plugins
+
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-ruby-sass');
+
+// Lint task
+
+gulp.task('lint', function() {
+	return gulp.src('/js/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
+});
+
+// Watch files for changes
+gulp.task('watch', function(){
+	gulp.watch('src/js/*.js', ['lint', 'scripts'])
+	gulp.watch('src/sass*.scss',  ['sass']);
+});
+
+
+// Minify and move JS files
+
+gulp.task('scripts', function() {
+	return gulp.src(['src/js/*.js',
+		'node_modules/requirejs/require.js',
+		'node_modules/responsive-nav/responsive-nav.js'])
+		.pipe(uglify())
+		.pipe(gulp.dest('assets/js'));	
+});
+
+// Compass Sass
+
+gulp.task( 'sass', function(){
+	return gulp.src('src/sass/styles.scss')
+	.pipe(sass({
+		compass:true,
+		style:'compressed'
+	}))
+	.on('error', function (err) {console.log(err.message); })
+	.pipe(gulp.dest('assets/css'))
+});
+
+// Default task
+
+gulp.task( 'default', ['lint', 'scripts', 'sass', 'watch']);
---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+// Configuration
+
+requirejs.config({
+	paths:
+	{
+		'jquery':'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+		'jqueryui':'http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css'
+	}, 
+	shim:
+	{
+		'jqueryui':['jquery']
+	}
+});
+
+
+//Responsive Nav
+
+require(['responsive-nav.min','jqueryui'], function()
+	{
+		responsiveNav('.nav-collapse');
+
+		$('.jquery-ui-accordian').accordion({
+			header:'h3'
+		})
+	});
+
+
+// Accordion
+
